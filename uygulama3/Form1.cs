6a6b8f8d54bc88f97aa01aff1c589e9d6e3e1f05
ennurdmbs <145125004+ennurdmbs@users.noreply.github.com>
2023-10-06 16:25:55 +0300

    Add files via upload

---
+++ uygulama3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte sayi1, sayi2;
+            sayi1 = Convert.ToByte(textBox1.Text);
+            sayi2 = Convert.ToByte(textBox2.Text);
+            if (sayi1 > sayi2)
+            {
+                MessageBox.Show("1. Sayı 2 den Büyüktür");
+            }
+            if (sayi1 == sayi2)
+            {
+                MessageBox.Show("Sayılar Birbirine Eşittir");
+            }
+            if (sayi1 < sayi2)
+            {
+                MessageBox.Show("2.Sayı 1den Küçüktür");
+            }
+        }
+    }
+}
